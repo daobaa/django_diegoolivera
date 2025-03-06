0f0b9929b027b265d2a93b0fb53500b456990c05
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-03-06 18:02:12 +0100

    new urls and views

--- ACTIVITAT_13/centre/urls.py
+++ ACTIVITAT_13/centre/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('students', views.students, name='students'),
-    path('teachers', views.teachers, name='teachers')
+    path('students/student/<str:pk>/', views.student, name='student'),
+    path('teachers', views.teachers, name='teachers'),
+    path('teachers/teacher/<str:pk>/', views.teacher, name='teacher')
 ]
--- ACTIVITAT_13/centre/views.py
+++ ACTIVITAT_13/centre/views.py
@@ -5,6 +5,11 @@ from django.template import loader
 def students(request):
     student = {"name":"Diego Andres", "surname1":"Olivera", "surname2":"Abarca", "mail":"2024_diego.olivera@iticbcn.cat", "course":"DAW2", "modules":"Tots"}
     return render(request, 'students.html', {"nom":student["name"], "cognom1":student["surname1"], "cognom2":student["surname2"], "correu":student["mail"], "curs":student["course"], "moduls":student["modules"]})
+
+def student(request):
+
 def teachers(request):
     professor = {"name":"Sergi", "surname1":"Franco", "surname2":"Gonzalez", "mail":"sfranco@iticbcn.cat", "course":"DAW2", "thutor":"Header", "modules":"M14"}
     return render(request, 'teachers.html', {"nom":professor["name"], "cognom1":professor["surname1"], "cognom2":professor["surname2"], "correu":professor["mail"], "curs":professor["course"], "tutor":professor["thutor"], "moduls_imp":professor["modules"]})
+
+def teacher(request):
