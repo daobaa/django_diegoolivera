2a4c480b7be57afffdbdb4cafeeac20477a09293
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-03-06 17:27:01 +0100

    Fixed relation index and other pages | css added

--- ACTIVITAT_13/centre/urls.py
+++ ACTIVITAT_13/centre/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_one, name='index_one'),
-    path('index', views.index, name='index'),
     path('students', views.students, name='students'),
     path('teachers', views.teachers, name='teachers')
 ]
--- ACTIVITAT_13/centre/views.py
+++ ACTIVITAT_13/centre/views.py
@@ -2,16 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def index_one(request):
-    return HttpResponse("Hello, world")
-def index(request):
-    professor = {"name": "Sergi", "surname1": "Franco", "mail":"sfranco@iticbcn.cat"}
-    template = loader.get_template('index.html')
-    data = template.render({"nom":professor["name"], "cognom1":professor["surname1"], "correu":professor["mail"]})
-    return HttpResponse(data)
 def students(request):
-    template = loader.get_template('students.html')
-    return HttpResponse(template.render())
+    student = {"name":"Diego Andres", "surname1":"Olivera", "surname2":"Abarca", "mail":"2024_diego.olivera@iticbcn.cat", "course":"DAW2", "modules":"Tots"}
+    return render(request, 'students.html', {"nom":student["name"], "cognom1":student["surname1"], "cognom2":student["surname2"], "correu":student["mail"], "curs":student["course"], "moduls":student["modules"]})
 def teachers(request):
-    template = loader.get_template('teachers.html')
-    return HttpResponse(template.render())
+    professor = {"name":"Sergi", "surname1":"Franco", "surname2":"Gonzalez", "mail":"sfranco@iticbcn.cat", "course":"DAW2", "thutor":"Header", "modules":"M14"}
+    return render(request, 'teachers.html', {"nom":professor["name"], "cognom1":professor["surname1"], "cognom2":professor["surname2"], "correu":professor["mail"], "curs":professor["course"], "tutor":professor["thutor"], "moduls_imp":professor["modules"]})
