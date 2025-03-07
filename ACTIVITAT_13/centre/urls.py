95b19ef37cb2d32a7220de1fd7c52f4c87d73cf1
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-03-07 19:34:00 +0100

    Dictionary used in views for individuals | urls corrected for multiple and individuals

--- ACTIVITAT_13/centre/urls.py
+++ ACTIVITAT_13/centre/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('students', views.students, name='students'),
+    path('', views.index, name='index'),
+    path('students/', views.students, name='students'),
     path('students/student/<str:pk>/', views.student, name='student'),
-    path('teachers', views.teachers, name='teachers'),
+    path('teachers/', views.teachers, name='teachers'),
     path('teachers/teacher/<str:pk>/', views.teacher, name='teacher')
 ]
